a13091dc8eb631bb82147f7634bf8e2aa26fe84a
Peter Tarsoly <ptarsoly@Peters-MacBook-Pro-4.local>
2019-03-07 18:43:24 -0500

    revised sensor fusion program for straight-oriented distance with sonars

--- sensor_fusion.py
+++ sensor_fusion.py
@@ -9,32 +9,56 @@ sensor_folder = 'sensor_data'
 
 numRows, numCols = 8, 4
 
-def gaussian(x, mu, sig):
-    return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
+
+def gaussian(x, mu, var):
+    return np.exp(-np.power(x - mu, 2.) / (2 * var))/np.sqrt(2*np.pi*var)
+
 
 # first number is average, second is variance
 # these characterize the normal distribution of distance at each square
-left_ = [[[0,0] for x in range(numCols)] for y in range(numRows)]
-right_samples = [[[0,0] for x in range(numCols)] for y in range(numRows)]
+left_cpt = []
+right_cpt = []
 
+print "loading left sonar CPTs"
 with open('left_sonar_straight_cpt.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
 
     for row in csv_reader:
         # norm is for each individual normal distribution
-        norm = row[0].split(', ')
+        norm = map(lambda x: float(x), row[0].split(', '))
+        left_cpt.append(norm)
+        print norm
+
+print "loading right sonar CPTs"
+with open('right_sonar_straight_cpt.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+
+    for row in csv_reader:
+        # norm is for each individual normal distribution
+        norm = map(lambda x: float(x), row[0].split(', '))
+        right_cpt.append(norm)
+        print norm
+
 
 # for sensor fusion
 
-# print "type in left sonar distance from wall: "
-# distance = input()
+print "type in left sonar distance from wall: "
+left_distance = input()
 
-# maxIndex = [0,0]
+print "type in right sonar distance from wall: "
+right_distance = input()
 
-# for row in range(numRows):
-#     for col in range(numCols):
-#         if gaussian(distance, np.average(left_samples[row][col]), np.sqrt(np.var(left_samples[row][col]))) > gaussian(distance, np.average(left_samples[maxIndex[0]][maxIndex[1]]), np.sqrt(np.var(left_samples[maxIndex[0]][maxIndex[1]]))):
-#             maxIndex[0] = row
-#             maxIndex[1] = col
+max_index = 0
 
-# print "most likely left distance: ", maxIndex[0]
+for row in range(numRows):
+    print "Coordinate", row
+    print "Left probability: ", gaussian(left_distance, np.average(left_cpt[row]), np.var(left_cpt[row]))
+    print "Right probability: ", gaussian(right_distance, np.average(right_cpt[row]), np.var(right_cpt[row]))
+    if gaussian(left_distance, np.average(left_cpt[row]), np.var(left_cpt[row]))*gaussian(right_distance, np.average(right_cpt[row]), np.var(right_cpt[row])) > gaussian(left_distance, np.average(left_cpt[max_index]), np.var(left_cpt[max_index]))*gaussian(right_distance, np.average(right_cpt[max_index]), np.var(right_cpt[max_index])):
+        max_index = row
+
+print "most likely distance coordinate: ", max_index
+print "most likely distance coordinate left CPT: ", left_cpt[max_index]
+print "most likely distance coordinate left probability: ", gaussian(left_distance, np.average(left_cpt[max_index]), np.var(left_cpt[max_index]))
+print "most likely distance coordinate right CPT: ", right_cpt[max_index]
+print "most likely distance coordinate right probability: ",gaussian(right_distance, np.average(right_cpt[max_index]), np.var(right_cpt[max_index]))
